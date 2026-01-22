6f9e855cb3511485e9a6640641134fd909096c52
ThomasDebek <tom.debek@gmail.com>
2026-01-22 18:45:22 +0100

    seed: created task

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,23 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# db/seeds.rb
+Task.destroy_all
+p "Created Tasks table"
+
+Task.create!(
+  title: "Learn Ruby",
+  description: "Basics of Ruby",
+  completed: false
+)
+
+Task.create!(
+  title: "Learn Rails",
+  description: "MVC, routing, controllers",
+  completed: true
+)
+
+Task.create!(
+  title: "Build task manager",
+  description: "CRUD + toggle",
+  completed: false
+)
+
+puts "Seeds created"
